ab3f3283e9b2dabb2491bb5737a1143b02cc4bc3
Ben Levy <benlevy1999@walla.co.il>
2023-10-08 22:48:16 +0300

    RENEW applications model
    ADD ApiService Application Layer
    FIX references for project layered

--- UserServiceClient/HttpClient/Core/UserServiceHttpClient.cs
+++ UserServiceClient/HttpClient/Core/UserServiceHttpClient.cs
@@ -1,7 +1,13 @@
-﻿using UserServiceClient.Abstractions;
+﻿using FluentResults;
+using UserService.Contracts.Requests;
+using UserServiceClient.Abstractions;
 
 namespace UserServiceClient.HttpClient.Core;
 
 public class UserServiceHttpClient : IUserServiceClient
 {
+    public Task<Result<RegisterRequest>> Register(RegisterRequest request, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
 }
