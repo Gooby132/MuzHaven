73ae088f645db8c94aebb0db4e73b8721c4e41d3
ernestromen <ernestromen1996@gmail.com>
2023-10-09 21:04:37 +0300

    added jwt tokken

--- ApiService/Controllers/WeatherForecastController.cs
+++ ApiService/Controllers/WeatherForecastController.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 namespace ApiService.Controllers
 {
     [ApiController]
@@ -33,8 +39,23 @@ namespace ApiService.Controllers
         [HttpPost]
         public IActionResult Post()
         {
-            string message = "This is a post action!";
-            return Ok(new { message });
+            // Generate a JWT token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("your-secret-key-for-real-what-about-now"); // Replace with your secret key
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "username"), // Replace with the user's username or identifier
+                    // You can add more claims as needed
+                }),
+                Expires = DateTime.UtcNow.AddHours(1), // Token expiration time
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new { message, token = tokenString });
         }
     }
 }
